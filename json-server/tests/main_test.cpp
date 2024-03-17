594ae6e61fb5f7346fce4d5b7857187c0c1deee1
MikePuzanov <mikepuzanov@mail.ru>
2024-03-18 00:18:42 +0300

    update test

--- json-server/tests/main_test.cpp
+++ json-server/tests/main_test.cpp
@@ -85,7 +85,7 @@ TEST_CASE("Index Exception", "[index_exception]") {
 }
 
 TEST_CASE("No array Exception", "[no_array_exception]") {
-    httplib::Client client(url, port);
+    httplib::Client client("localhost", 18080);
     nlohmann::json addRequest = {{}, {1, "zs", {{"v","ret"}, {"hl",1}}}};
     client.Post("/add", addRequest.dump(), "application/json");
 
